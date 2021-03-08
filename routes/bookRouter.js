e7cfcf12b7eab3dd70b1cc3015e293847e919638
Prerna <prerna1998.ps@gmail.com>
2021-03-08 11:30:32 +0530

    Add Book

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const Books = require('../models/book');
 
 const bookRouter = express.Router();
+
+bookRouter.use(bodyParser.json());
 
 bookRouter.route('/books')
     .get((req, res) => {
@@ -14,7 +18,21 @@ bookRouter.route('/books')
                 res.send(err);
             })
         ;
-    }
-);
+    })
+    .post((req,res) => {
+        Books.create(req.body)
+        .then( (book) =>{
+            console.log("book Created ", book)
+            res.statusCode=200
+            res.setHeader('Content-Type', 'application/json')
+            res.json(book)
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send(err);
+        })
+    });
+
+    
 
 module.exports = bookRouter;
